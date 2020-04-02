6fe85a9ad2717f0f89d02ab658c3bdce9b6391ad
Christopher Reilly <christopher_reilly@homedepot.com>
2020-04-02 17:05:17 -0500

    initial commits

--- README.md
+++ README.md
@@ -1,68 +1,36 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-## Available Scripts
+# ATN Egg Hunt
+A fun application for hunting down easter eggs hidden throughout the ATN
 
-In the project directory, you can run:
+## Images
+All images for the rooms are in the `public/images` folder. 
 
-### `yarn start`
+## Eggs
+The configuration for where the eggs are and how the rooms connect are located in `src/maps.json`.  
+The json is an object with more location objects. Each object should have a unique key and the following fields:
+- name: used for keeping track of which eggs were found. can be arbitrary
+- imageName: path to the image in the public/images folder. When built, the path will be `./images/IMAGE_NAME.png`
+- up/down/left/right/elevatorUp/elevatorDown: The other locations that this location maps to. The value should be the key for the corresponding location.
+    - So if you want the user to click the up arrow to go fron 1N1 to 1N2, then the 1N1 object should have `"up": "1N2"`
+    - These are optional. If a location won't have a left arrow, then don't include the left property
+- eggs: The array of eggs for a location. Each egg object should have the following values
+    - eggX: the X coordinate of the egg
+    - eggY: the Y coordinate of the egg
+    - eggRadius: the radius of the egg
 
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+## Adding Eggs
+To add eggs, you just need to add an egg object to the locations egg array in `src/map.json`. There is a built in tool to assist with getting the egg information.  
+In `src/App.jsx`, there is a variable `HUNT_MODE` setting this to false will add extra elements to the UI to assist in getting the egg information
+- A draggable red circle will be present. Drag this over the eggs you want to add.
+- A slider will be at the top left of the screen. This adjusts the size of the red circle.
+- A text box will be at the top left of the screen. This will contain the eggX, eggY, and eggRadius for the red circle.
+Drag the red circle over the egg, adjust the radius, and copy the values in the text box into the eggs array in `maps.json`.  
+Note: Be sure to set `HUNT_MODE` to true before releasing the app out to the players.
 
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
-
-### `yarn test`
-
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `yarn build`
-
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `yarn build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+## Behavior
+`src/maps.json` is loaded and parsed in `src/App.jsx`. The starting location is defaulted to the `ENTRANCE` value.  
+Based on the up/down/left/right/elevatorUp/elevatorDown properties of the location, the corresponding arrows are made visible on the image. Based on the eggs array, invisible circles are placed throughout the image.  
+When one of the invisible egg circles is pressed, the onClick handler for that circle increments the score, and marks itself as found in the foundEggs array. Any eggs in this array are rendered as Stars instead of invisible circles.  
+Whenever an arrow is clicked, the onClick handler updates the currentLocation based on the corresponding direction clicked.  
+When the submit button is clicked, the name and score are posted to the backend. There's no real security right now because this is a simple, fun little app.
---
+++ src/App.jsx
@@ -0,0 +1,352 @@
+import React, {useState, useEffect} from 'react';
+import {
+    Circle,
+    Image,
+    Layer,
+    Rect,
+    Stage,
+    Star,
+    Text
+} from 'react-konva'
+import useImage from 'use-image';
+import maps from './maps.json'
+
+// Turn off HUNT_MODE to enable tools to get the x/y/radius of the eggs
+const HUNT_MODE = true
+
+// Preload all the maps
+Object.keys(maps).forEach(key => {
+  let image = new window.Image()
+  image.src = maps[key].imageName
+  maps[key].image = image
+})
+
+const height = window.innerHeight
+const width = window.innerWidth
+
+function App() {
+  const [status, setStatus] = useState('loading')
+
+  const [name, setName] = useState('')
+  const [score, setScore] = useState(0)
+  const [foundEggs, setFoundEggs] = useState([])
+
+  const [scale, setScale] = useState(0.2)
+  const [image, setImage] = useState()
+  const [imageX, setImageX] = useState(0)
+  const [currentLocation, setCurrentLocation] = useState(maps.ENTRANCE)
+
+  const [eggX, setEggX] = useState(576)
+  const [eggY, setEggY] = useState(446)
+  const [eggRadius, setEggRadius] = useState(30)
+  
+  const [landingPage] = useImage('ATNEggHunt.png')
+  const [arrowUp] = useImage('ArrowUp.png')
+  const [arrowDown] = useImage('ArrowDown.png')
+  const [arrowLeft] = useImage('ArrowLeft.png')
+  const [arrowRight] = useImage('ArrowRight.png')
+  const [elevatorUp] = useImage('ElevatorUp.png')
+  const [elevatorDown] = useImage('ElevatorDown.png')
+
+  let handleImageDrag = event => {
+    setEggX((event.target.attrs.x- imageX) / scale )
+    setEggY((event.target.attrs.y  / scale))
+  }
+
+  useEffect(() => {
+    if(landingPage){
+      setStatus('landing')
+      setImage(landingPage)
+    }
+  }, [landingPage])
+
+
+  useEffect(() => {
+    if(status === 'hunting'){
+      alert(`Homer has hidden eggs all over the ATN! Can you find them all?\nClick on any eggs that you can spot. Use the arrows at the bottom of the screen to navigate the building.\nClick "Submit Score" when you think you've found them all.`)
+    }
+  }, [status])
+
+  useEffect(() => {
+    switch(status) {
+      case 'landing':
+        setImage(landingPage); break
+      case 'hunting': setImage(currentLocation.image); break
+      default:
+    }
+  }, [status, currentLocation, landingPage])
+
+  // On first mount, check if we need to load up a map
+  useEffect(() => {
+    if (image) {
+      let scaleX = width / image.width
+      let scaleY = height / image.height
+      let scale = Math.min(scaleX, scaleY)
+      let imageX = (window.innerWidth/2) - (image.width * scale * 0.5)
+      setScale(scale)
+      setImageX(imageX)
+    }
+  }, [image])
+
+  let changeLocation = (newLocationName) => {
+    setCurrentLocation(maps[newLocationName])
+  }
+
+  if (status === 'loading') {
+    return (<div>Loading</div>)
+  } else if (status === 'landing'  || status === 'checking name') {
+    return (
+      <>
+      <input
+        type="text"
+        placeholder="Enter your name/ldap"
+        value={name}
+        autoFocus
+        style={{
+          position: "absolute",
+          top: `${870 * scale}px`,
+          left: `${imageX + (image.width / 2 * scale) - (200* scale)}px`,
+          zIndex: 999,
+          height: `${70 * scale}px`,
+          width: `${400 * scale}px`,
+          fontSize: `${40 * scale}px`
+        }}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+          type="Button"
+          value="Start!"
+          style={{
+            position: "absolute",
+            top: `${1000 * scale}px`,
+            left: `${imageX + (image.width / 2 * scale) - (150* scale)}px`,
+            zIndex: 999,
+            height: `${120 * scale}px`,
+            width: `${300 * scale}px`,
+            fontSize: `${90 * scale}px`,
+            background: `${name === '' ? '' : 'yellow'}`
+          }}
+          disabled={name === ''}
+          onClick={() => setStatus('hunting')}
+      /> 
+      <Stage
+        width={window.innerWidth}
+        height={window.innerHeight}
+      >
+        <Layer>
+          <Image
+            image={image}
+            x={imageX}
+    
+            scaleX={scale}
+            scaleY={scale}
+          />
+        </Layer>
+      </Stage>
+      </>
+    )
+  } else if (status === 'hunting') {
+    return (<>
+      {
+        HUNT_MODE ?
+        <>
+        <input
+          type="Button"
+          value="Submit Score"
+          style={{
+            position: "absolute",
+            top: `60px`,
+            left: `10px`,
+            zIndex: 999,
+            height: `${60}px`,
+            width: `${200}px`,
+            fontSize: `${30}px`,
+            background: 'yellow'
+          }}
+          onClick={() => {
+            fetch('https://atn-egg-hunt.apps-np.homedepot.com/newScore',
+            {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({name, score})
+            })
+            .then((resp) => {
+              if (resp.ok) {
+                alert(`${name} submitted a score of ${score} eggs!\nThanks for playing!`)
+                setStatus('landing')
+                setName('')
+                setScore(0)
+                setFoundEggs([])
+                setCurrentLocation(maps.ENTRANCE)
+              }
+            })
+          }}
+        /> 
+        </> :
+        <>
+          <textarea 
+              style={{position: "absolute", top: '0px', left: '0px', zIndex: 999}}
+            rows="4" cols="13" readOnly
+            value={`{"eggX": ${eggX.toFixed(0)},\n"eggY": ${eggY.toFixed(0)},\n"eggRadius": ${eggRadius}}`}
+          />
+          <input
+              type="range"
+              min="1"
+              max="200"
+              value={eggRadius}
+              style={{position: "absolute", top: '70px', left: '0px', zIndex: 999}}
+              onChange={(e) => setEggRadius(e.target.value)}
+          /> 
+        </>
+      }
+      <Stage
+        width={window.innerWidth}
+        height={window.innerHeight}
+      >
+        <Layer>
+          <Rect 
+            width={width}
+            height={height}
+            fill='#999999'
+          />
+          <Image
+            image={image}
+            x={imageX}
+    
+            scaleX={scale}
+            scaleY={scale}
+          />
+        </Layer>
+        <Layer>
+          <Rect 
+            x={5}
+            stroke={'#555'}
+            strokeWidth={5}
+            fill={'#ddd'}
+            width={280}
+            height={50}
+            shadowColor={'black'}
+            shadowBlur={10}
+            shadowOffsetX={10}
+            shadowOffsetY={10}
+            shadowOpacity={0.2}
+            cornerRadius={10}
+          />
+          <Text
+            x={15}
+            y={10}
+            wrap
+            text={`Current Score: ${score}`}
+            fontSize={30}
+          />
+    
+          {currentLocation.up && 
+          <Image
+            image={arrowUp}
+            x={window.innerWidth * 0.5 - (arrowUp ? arrowUp.width : 0) * 0.5 * 0.1}
+            y={window.innerHeight * 0.8 - (arrowUp ? arrowUp.height : 0) * 0.5 * 0.1}
+            scaleX={0.1}
+            scaleY={0.1}
+            onClick={() => changeLocation(currentLocation.up)}
+          />}
+          { currentLocation.down && 
+          <Image
+            image={arrowDown}
+            x={window.innerWidth * 0.5 - (arrowDown ? arrowDown.width : 0) * 0.5 * 0.1}
+            y={window.innerHeight * 0.8 + (arrowDown ? arrowDown.height : 0) * 0.5 * 0.1}
+            scaleX={0.1}
+            scaleY={0.1}
+            onClick={() => changeLocation(currentLocation.down)}
+          />}
+          { currentLocation.left && 
+          <Image
+            image={arrowLeft}
+            x={window.innerWidth * 0.47 - (arrowLeft ? arrowLeft.width : 0) * 0.5 * 0.1}
+            y={window.innerHeight * 0.78 + (arrowLeft ? arrowLeft.height : 0) * 0.5 * 0.1}
+            scaleX={0.1}
+            scaleY={0.1}
+            onClick={() => changeLocation(currentLocation.left)}
+          />}
+          { currentLocation.right && 
+          <Image
+            image={arrowRight}
+            x={window.innerWidth * 0.53 - (arrowRight ? arrowRight.width : 0) * 0.5 * 0.1}
+            y={window.innerHeight * 0.78 + (arrowRight ? arrowRight.height : 0) * 0.5 * 0.1}
+            scaleX={0.1}
+            scaleY={0.1}
+            onClick={() => changeLocation(currentLocation.right)}
+          />}
+
+          { currentLocation.elevatorUp && 
+          <Image
+            image={elevatorUp}
+            x={window.innerWidth * 0.47 - (arrowLeft ? arrowLeft.width : 0) * 0.5 * 0.1}
+            y={window.innerHeight * 0.74 - (arrowUp ? arrowUp.height : 0) * 0.5 * 0.1}
+            scaleX={0.5}
+            scaleY={0.5}
+            onClick={() => changeLocation(currentLocation.elevatorUp)}
+          />}
+          { currentLocation.elevatorDown && 
+          <Image
+            image={elevatorDown}
+            x={window.innerWidth * 0.53 - (elevatorDown ? elevatorDown.width : 0) * 0.5 * 0.1}
+            y={window.innerHeight * 0.76 - (arrowUp ? arrowUp.height : 0) * 0.5 * 0.1}
+            scaleX={0.5}
+            scaleY={0.5}
+            onClick={() => changeLocation(currentLocation.elevatorDown)}
+          />}
+    
+          {// If in HUNT_MODE, put invisible circles on unfound eggs and stars on found eggs
+            HUNT_MODE ?
+              currentLocation.eggs.map((egg, i) => (
+                foundEggs.indexOf(`${currentLocation.name}egg${i}`) === -1 ?
+                  <Circle 
+                    x={ imageX + (egg.eggX * scale)}
+                    y={ (egg.eggY * scale)}
+                    radius={ egg.eggRadius * scale}
+                    onClick={() => {
+                      setScore(score+1)
+                      setFoundEggs([`${currentLocation.name}egg${i}`, ...foundEggs])
+                    }}
+                    key={`${currentLocation.name}egg${i}`}
+                  />
+                :
+                  <Star 
+                      x={ imageX + (egg.eggX * scale)}
+                      y={ (egg.eggY * scale)}
+                      innerRadius={ egg.eggRadius * scale * 0.7}
+                      outerRadius={ egg.eggRadius * scale * 1.5}
+                      rotation={10}
+                      numPoints={5}
+                      fill='#F96302'
+                      stroke='black'
+                      strokeWidth={2 * scale}
+                      key={`${currentLocation.name}egg${i}`}
+                  /> 
+                )
+              )
+            :
+              <Circle 
+                x={ imageX + (eggX * scale)}
+                y={ (eggY * scale)}
+                radius={ eggRadius * scale}
+    
+                draggable
+                onDragMove={handleImageDrag}
+    
+                stroke={ 'red'}
+                strokeWidth={ 2}
+              />
+          }
+        </Layer>
+      </Stage>
+      </>);
+  } else {
+    return (<div>Something done broke</div>)
+  }
+  
+}
+
+export default App;
